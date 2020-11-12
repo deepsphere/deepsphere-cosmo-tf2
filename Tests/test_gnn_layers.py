b3b00bf1f3c6ca6e83465ca8bb8abcff614abb5d
Janis <jafluri@student.ethz.ch>
2020-11-12 07:50:02 +0100

    added graph convolutions and tests

---
+++ Tests/test_gnn_layers.py
@@ -0,0 +1,148 @@
+from deepsphere import gnn_layers
+
+import tensorflow as tf
+
+import healpy as hp
+
+import numpy as np
+
+import pytest
+
+def test_Chebyshev():
+    # this is the result from Deepsphere with tf 1.x
+    result = np.array([[[-1.0755178 , -0.38834727, -1.8771361 ],
+                        [-0.717345  , -0.05060194,  0.5165049 ],
+                        [-0.26436844, -1.9551289 ,  1.5731683 ]],
+
+                       [[ 0.20308694,  0.18807065,  0.5316967 ],
+                        [-0.15023257,  0.5063435 , -2.1237612 ],
+                        [ 0.8385707 , -0.05848193, -0.99261296]],
+
+                       [[-0.72604334, -1.1806095 , -1.2536838 ],
+                        [ 0.94199276, -0.45402163, -0.37584513],
+                        [-0.5897862 , -0.99283355,  0.39791816]],
+
+                       [[ 0.35315517, -0.69710857,  0.89600056],
+                        [-1.2446954 ,  0.17451617,  2.5738995 ],
+                        [-0.48410773, -1.3228154 ,  0.73148715]],
+
+                       [[-0.47506812,  0.07938811, -0.7293904 ],
+                        [ 0.9361354 ,  0.5696295 ,  1.4792094 ],
+                        [-0.5101731 ,  0.43999135,  0.34349984]]], dtype=np.float32)
+
+    # create the layer
+    tf.random.set_seed(11)
+    L = tf.random.normal(shape=(3, 3), seed=11)
+    # make sym
+    L = tf.matmul(L, tf.transpose(L))
+    x = tf.random.normal(shape=(5, 3, 7), seed=12)
+    Fout = 3
+    K = 4
+
+
+    # create the layer
+    stddev = 1 / np.sqrt(7 * (K + 0.5) / 2)
+    initializer = tf.initializers.RandomNormal(stddev=stddev, seed=13)
+    cheb = gnn_layers.Chebyshev(L=L.numpy(), Fout=Fout, K=K, initializer=initializer)
+    new = cheb(x)
+
+    assert np.all(np.abs(new.numpy() - result) < 1e-5)
+
+    # same with new activation
+    cheb = gnn_layers.Chebyshev(L=L.numpy(), Fout=Fout, K=K, initializer=initializer, activation="linear")
+    new = cheb(x)
+
+    assert np.all(np.abs(new.numpy() - result) < 1e-5)
+
+def test_Monimials():
+    # this is the result from Deepsphere with tf 1.x
+    result = np.array([[[ 0.04206353,  0.46168754,  0.10546149],
+                        [-0.5492798 , -0.32608002,  0.5628096 ],
+                        [-0.11329696, -0.7900159 ,  0.92530084]],
+
+                       [[ 0.06915615,  0.03369189,  0.0245935 ],
+                        [-0.89208144, -0.11626951, -0.10967396],
+                        [ 0.01909873, -0.16593638, -0.1462554 ]],
+
+                       [[-0.29119226, -0.12377091, -0.0128078 ],
+                        [ 0.36727118,  0.30154356, -0.02591037],
+                        [-0.23363924, -0.14655769,  0.3258103 ]],
+
+                       [[ 0.00471622, -0.03371258,  0.00214787],
+                        [ 0.31400114, -0.57628125,  1.5108933 ],
+                        [ 0.09324764, -0.75300777,  0.40933472]],
+
+                       [[ 0.12954447,  0.06049673,  0.15058015],
+                        [ 0.38768154, -0.24916826,  0.43720144],
+                        [-0.1512235 ,  0.01706326,  0.14433491]]], dtype=np.float32)
+
+    # create the layer
+    tf.random.set_seed(11)
+    L = tf.random.normal(shape=(3, 3), seed=11)
+    # make sym
+    L = tf.matmul(L, tf.transpose(L))
+    x = tf.random.normal(shape=(5, 3, 7), seed=12)
+    Fout = 3
+    K = 4
+
+    # create the layer
+    stddev = 0.1
+    initializer = tf.initializers.RandomNormal(stddev=stddev, seed=13)
+    mon = gnn_layers.Monomial(L=L.numpy(), Fout=Fout, K=K, initializer=initializer,
+                              activation=tf.keras.activations.linear)
+    new = mon(x)
+
+    assert np.all(np.abs(new.numpy() - result) < 1e-5)
+
+    # same with new actiation
+    mon = gnn_layers.Monomial(L=L.numpy(), Fout=Fout, K=K, initializer=initializer,
+                              activation="elu")
+    new_1 = mon(x)
+
+    assert new_1.shape == new.shape
+
+def test_GCNN_ResidualLayer():
+    # we get a random map to pool
+    n_pix = hp.nside2npix(4)
+    np.random.seed(11)
+    m_in = np.random.normal(size=[3, n_pix, 7])
+
+    # check exception
+    with pytest.raises(IOError):
+        _ = gnn_layers.GCNN_ResidualLayer("juhu", dict())
+
+    # layer definition
+    layer_type = "CHEBY"
+    layer_kwargs = {"L": np.eye(n_pix, dtype=np.float64),
+                    "K": 5,
+                    "activation": tf.keras.activations.relu,
+                    "regularizer": tf.keras.regularizers.l1}
+
+    res_layer = gnn_layers.GCNN_ResidualLayer(layer_type=layer_type, layer_kwargs=layer_kwargs,
+                                              activation=tf.keras.activations.relu)
+    out = res_layer(m_in)
+
+    assert out.numpy().shape == (3, n_pix, 7)
+
+    # same with batch norms
+    res_layer = gnn_layers.GCNN_ResidualLayer(layer_type=layer_type, layer_kwargs=layer_kwargs,
+                                              activation=tf.keras.activations.relu, use_bn=True)
+    out = res_layer(m_in)
+
+    assert out.numpy().shape == (3, n_pix, 7)
+
+    res_layer = gnn_layers.GCNN_ResidualLayer(layer_type=layer_type, layer_kwargs=layer_kwargs,
+                                              activation=tf.keras.activations.relu, use_bn=True,
+                                              norm_type="layer_norm", bn_kwargs={"axis": (1,2)})
+    out = res_layer(m_in)
+
+    assert out.numpy().shape == (3, n_pix, 7)
+
+    with pytest.raises(ValueError):
+        res_layer = gnn_layers.GCNN_ResidualLayer(layer_type=layer_type, layer_kwargs=layer_kwargs,
+                                                  activation=tf.keras.activations.relu, use_bn=True,
+                                                  norm_type="moving_norm")
+
+
+
+
---
+++ deepsphere/gnn_layers.py
@@ -0,0 +1,363 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Layer
+
+import numpy as np
+
+from scipy import sparse
+from scipy.sparse.linalg import eigsh
+
+# local imports
+from . import utils
+
+
+class Chebyshev(Layer):
+    """
+    A graph convolutional layer using the Chebyshev approximation
+    """
+
+    def __init__(self, L, K, Fout=None, initializer=None, activation=None, **kwargs):
+        """
+        Initializes the graph convolutional layer, assuming the input has dimension (B, M, F)
+        :param L: The graph Laplacian (MxM), as numpy array
+        :param K: Order of the polynomial to use
+        :param Fout: Number of features (channels) of the output, default to number of input channels
+        :param initializer: initializer to use for weight initialisation
+        :param activation: the activation function to use after the layer, defaults to linear
+        :param kwargs: additional keyword arguments passed on to add_weight
+        """
+
+        # This is necessary for every Layer
+        super(Chebyshev, self).__init__(name='')
+
+        # save necessary params
+        self.L = L
+        self.K = K
+        self.Fout = Fout
+        self.initializer = initializer
+        if activation is None or callable(activation):
+            self.activation = activation
+        elif hasattr(tf.keras.activations, activation):
+            self.activation = getattr(tf.keras.activations, activation)
+        else:
+            raise ValueError(f"Could not find activation <{activation}> in tf.keras.activations...")
+        self.kwargs = kwargs
+
+        # Rescale Laplacian and store as a TF sparse tensor. Copy to not modify the shared L.
+        L = sparse.csr_matrix(L)
+        lmax = 1.02 * eigsh(L, k=1, which='LM', return_eigenvectors=False)[0]
+        L = utils.rescale_L(L, lmax=lmax, scale=0.75)
+        L = L.tocoo()
+        indices = np.column_stack((L.row, L.col))
+        L = tf.SparseTensor(indices, L.data, L.shape)
+        self.sparse_L = tf.sparse.reorder(L)
+
+    def build(self, input_shape):
+        """
+        Build the weights of the layer
+        :param input_shape: shape of the input, batch dim has to be defined
+        :return: the kernel variable to train
+        """
+
+        # get the input shape
+        Fin = int(input_shape[-1])
+
+        # get Fout if necessary
+        if self.Fout is None:
+            Fout = Fin
+        else:
+            Fout = self.Fout
+
+        if self.initializer is None:
+            # Filter: Fin*Fout filters of order K, i.e. one filterbank per output feature.
+            stddev = 1 / np.sqrt(Fin * (self.K + 0.5) / 2)
+            initializer = tf.initializers.TruncatedNormal(stddev=stddev)
+            self.kernel = self.add_weight("kernel", shape=[self.K * Fin, Fout],
+                                          initializer=initializer, **self.kwargs)
+        else:
+            self.kernel = self.add_weight("kernel", shape=[self.K * Fin, Fout],
+                                          initializer=self.initializer, **self.kwargs)
+
+        # we cast the sparse L to the current backend type
+        if tf.keras.backend.floatx() == 'float32':
+            self.sparse_L = tf.cast(self.sparse_L, tf.float32)
+        if tf.keras.backend.floatx() == 'float64':
+            self.sparse_L = tf.cast(self.sparse_L, tf.float64)
+
+    def compute_output_shape(self, input_shape):
+        """
+        Computes the output shape of the layer given an input shape
+        :param input_shape: shape of the input
+        :return: shape of the output
+        """
+        return [int(input_shape[0]), int(self.Fout)]
+
+    def call(self, input_tensor, *args, **kwargs):
+        """
+        Calls the layer on a input tensor
+        :param input_tensor: input of the layer shape (batch, nodes, channels)
+        :param args: further arguments
+        :param kwargs: further keyword arguments
+        :return: the output of the layer
+        """
+
+        # shapes, this fun is necessary since sparse_matmul_dense in TF only supports
+        # the multiplication of 2d matrices, therefore one has to do some weird reshaping
+        # this is not strictly necessary but leads to a huge performance gain...
+        # See: https://arxiv.org/pdf/1903.11409.pdf
+        N, M, Fin = input_tensor.get_shape()
+        N, M, Fin = int(N), int(M), int(Fin)
+
+        # get Fout if necessary
+        if self.Fout is None:
+            Fout = Fin
+        else:
+            Fout = self.Fout
+
+        # Transform to Chebyshev basis
+        x0 = tf.transpose(input_tensor, perm=[1, 2, 0])  # M x Fin x N
+        x0 = tf.reshape(x0, [M, Fin * N])  # M x Fin*N
+
+        # list for stacking
+        stack = [x0]
+
+        if self.K > 1:
+            x1 = tf.sparse.sparse_dense_matmul(self.sparse_L, x0)
+            stack.append(x1)
+        for k in range(2, self.K):
+            x2 = 2 * tf.sparse.sparse_dense_matmul(self.sparse_L, x1) - x0  # M x Fin*N
+            stack.append(x2)
+            x0, x1 = x1, x2
+        x = tf.stack(stack, axis=0)
+        x = tf.reshape(x, [self.K, M, Fin, N])  # K x M x Fin x N
+        x = tf.transpose(x, perm=[3, 1, 2, 0])  # N x M x Fin x K
+        x = tf.reshape(x, [N * M, Fin * self.K])  # N*M x Fin*K
+        # Filter: Fin*Fout filters of order K, i.e. one filterbank per output feature.
+        x = tf.matmul(x, self.kernel)  # N*M x Fout
+        x = tf.reshape(x, [N, M, Fout])  # N x M x Fout
+
+        if self.activation is not None:
+            x = self.activation(x)
+
+        return x
+
+
+class Monomial(Layer):
+    """
+    A graph convolutional layer using Monomials
+    """
+    def __init__(self, L, K, Fout=None, initializer=None, activation=None, **kwargs):
+        """
+        Initializes the graph convolutional layer, assuming the input has dimension (B, M, F)
+        :param L: The graph Laplacian (MxM), as numpy array
+        :param K: Order of the polynomial to use
+        :param Fout: Number of features (channels) of the output, default to number of input channels
+        :param initializer: initializer to use for weight initialisation
+        :param activation: the activation function to use after the layer, defaults to linear
+        :param kwargs: additional keyword arguments passed on to add_weight
+        """
+
+        # This is necessary for every Layer
+        super(Monomial, self).__init__(name='')
+
+        # save necessary params
+        self.L = L
+        self.K = K
+        self.Fout = Fout
+        self.initializer = initializer
+        if activation is None or callable(activation):
+            self.activation = activation
+        elif hasattr(tf.keras.activations, activation):
+            self.activation = getattr(tf.keras.activations, activation)
+        else:
+            raise ValueError(f"Could not find activation <{activation}> in tf.keras.activations...")
+        self.kwargs = kwargs
+
+        # Rescale Laplacian and store as a TF sparse tensor. Copy to not modify the shared L.
+        L = sparse.csr_matrix(L)
+        lmax = 1.02 * eigsh(L, k=1, which='LM', return_eigenvectors=False)[0]
+        L = utils.rescale_L(L, lmax=lmax)
+        L = L.tocoo()
+        indices = np.column_stack((L.row, L.col))
+        L = tf.SparseTensor(indices, L.data, L.shape)
+        self.sparse_L = tf.sparse.reorder(L)
+
+    def build(self, input_shape):
+        """
+        Build the weights of the layer
+        :param input_shape: shape of the input, batch dim has to be defined
+        """
+
+        # get the input shape
+        Fin = int(input_shape[-1])
+
+        # get Fout if necessary
+        if self.Fout is None:
+            Fout = Fin
+        else:
+            Fout = self.Fout
+
+        if self.initializer is None:
+            # Filter: Fin*Fout filters of order K, i.e. one filterbank per output feature.
+            initializer = tf.initializers.TruncatedNormal(stddev=0.1)
+            self.kernel = self.add_weight("kernel", shape=[self.K * Fin, Fout],
+                                          initializer=initializer, **self.kwargs)
+        else:
+            self.kernel = self.add_weight("kernel", shape=[self.K * Fin, Fout],
+                                          initializer=self.initializer, **self.kwargs)
+
+        # we cast the sparse L to the current backend type
+        if tf.keras.backend.floatx() == 'float32':
+            self.sparse_L = tf.cast(self.sparse_L, tf.float32)
+        if tf.keras.backend.floatx() == 'float64':
+            self.sparse_L = tf.cast(self.sparse_L, tf.float64)
+
+    def compute_output_shape(self, input_shape):
+        """
+        Computes the output shape of the layer given an input shape
+        :param input_shape: shape of the input
+        :return: shape of the output
+        """
+        return [int(input_shape[0]), int(self.Fout)]
+
+    def call(self, input_tensor, *args, **kwargs):
+        """
+        Calls the layer on a input tensor
+        :param input_tensor: input of the layer shape (batch, nodes, channels)
+        :param args: further arguments
+        :param kwargs: further keyword arguments
+        :return: the output of the layer
+        """
+
+        # shapes, this fun is necessary since sparse_matmul_dense in TF only supports
+        # the multiplication of 2d matrices, therefore one has to do some weird reshaping
+        # this is not strictly necessary but leads to a huge performance gain...
+        # See: https://arxiv.org/pdf/1903.11409.pdf
+        N, M, Fin = input_tensor.get_shape()
+        N, M, Fin = int(N), int(M), int(Fin)
+
+        # get Fout if necessary
+        if self.Fout is None:
+            Fout = Fin
+        else:
+            Fout = self.Fout
+
+        # Transform to monomial basis.
+        x0 = tf.transpose(input_tensor, perm=[1, 2, 0])  # M x Fin x N
+        x0 = tf.reshape(x0, [M, Fin * N])  # M x Fin*N
+
+        # list for stacking
+        stack = [x0]
+
+        for k in range(1, self.K):
+            x1 = tf.sparse.sparse_dense_matmul(self.sparse_L, x0)  # M x Fin*N
+            stack.append(x1)
+            x0 = x1
+
+        x = tf.stack(stack, axis=0)
+        x = tf.reshape(x, [self.K, M, Fin, N])  # K x M x Fin x N
+        x = tf.transpose(x, perm=[3, 1, 2, 0])  # N x M x Fin x K
+        x = tf.reshape(x, [N * M, Fin * self.K])  # N*M x Fin*K
+        # Filter: Fin*Fout filters of order K, i.e. one filterbank per output feature.
+        x = tf.matmul(x, self.kernel)  # N*M x Fout
+        x = tf.reshape(x, [N, M, Fout])  # N x M x Fout
+
+        if self.activation is not None:
+            x = self.activation(x)
+
+        return x
+
+
+class GCNN_ResidualLayer(Model):
+    """
+    A generic residual layer of the form
+    in -> layer -> layer -> out + in
+    with optional batchnorm in the end
+    """
+
+    def __init__(self, layer_type, layer_kwargs, activation=None, act_before=False, use_bn=False,
+                 norm_type="batch_norm", bn_kwargs=None):
+        """
+        Initializes the residual layer with the given argument
+        :param layer_type: The layer type, either "CHEBY" or "MONO" for chebychev or monomials
+        :param layer_kwargs: A dictionary with the inputs for the layer
+        :param activation: activation function to use for the res layer
+        :param act_before: use activation before skip connection
+        :param use_bn: use batchnorm inbetween the layers
+        :param norm_type: type of batch norm, either batch_norm for normal batch norm or layer_norm for
+                          tf.keras.layers.LayerNormalization
+        :param bn_kwargs: An optional dictionary containing further keyword arguments for the normalization layer
+        """
+        # This is necessary for every Layer
+        super(GCNN_ResidualLayer, self).__init__(name='')
+
+        # save variables
+        self.layer_type = layer_type
+        self.layer_kwargs = layer_kwargs
+        if activation is None or callable(activation):
+            self.activation = activation
+        elif hasattr(tf.keras.activations, activation):
+            self.activation = getattr(tf.keras.activations, activation)
+        else:
+            raise ValueError(f"Could not find activation <{activation}> in tf.keras.activations...")
+        self.act_before = act_before
+        self.use_bn = use_bn
+        self.norm_type = norm_type
+        # set the default axis if necessary
+        if bn_kwargs is None:
+                self.bn_kwargs = {"axis": -1}
+        else:
+            self.bn_kwargs = bn_kwargs
+            if "axis" not in self.bn_kwargs and norm_type != "moving_norm":
+                self.bn_kwargs.update({"axis": -1})
+
+        if self.layer_type == "CHEBY":
+            self.layer1 = Chebyshev(**self.layer_kwargs)
+            self.layer2 = Chebyshev(**self.layer_kwargs)
+        elif self.layer_type == "MONO":
+            self.layer1 = Monomial(**self.layer_kwargs)
+            self.layer2 = Monomial(**self.layer_kwargs)
+        else:
+            raise IOError(f"Layertype not understood: {self.layer_type}")
+
+
+        if use_bn:
+            if norm_type == "layer_norm":
+                self.bn1 = tf.keras.layers.LayerNormalization(**self.bn_kwargs)
+                self.bn2 = tf.keras.layers.LayerNormalization(**self.bn_kwargs)
+            elif norm_type == "batch_norm":
+                self.bn1 = tf.keras.layers.BatchNormalization(**self.bn_kwargs)
+                self.bn2 = tf.keras.layers.BatchNormalization(**self.bn_kwargs)
+            else:
+                raise ValueError(f"norm_type <{norm_type}> not understood!")
+
+    def call(self, input_tensor, training=False, *args, **kwargs):
+        """
+        Calls the layer on a input tensorf
+        :param input_tensor: The input of the layer
+        :param training: wheter we are training or not
+        :param args: further arguments (ignored)
+        :param kwargs: further keyword arguments (ignored)
+        :return: the output of the layer
+        """
+        x = self.layer1(input_tensor)
+
+        # bn
+        if self.use_bn:
+            x = self.bn1(x, training=training)
+
+        # 2nd layer
+        x = self.layer2(x)
+
+        # bn
+        if self.use_bn:
+            x = self.bn2(x, training=training)
+
+        # deal with the activation
+        if self.activation is None:
+            return x + input_tensor
+
+        if self.act_before:
+            return self.activation(x) + input_tensor
+        else:
+            return self.activation(x + input_tensor)
